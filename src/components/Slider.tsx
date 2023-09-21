9aed09e7e0c823e920da6870661f5a7a6cca17d9
Tomislav Pandzic <tome91@windowslive.com>
2023-09-21 11:04:51 +0200

    first commit

---
+++ src/components/Slider.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+
+const data = [
+  {
+    id: 1,
+    title: 'Always fresh, always crispy & always hot',
+    image: '/slide1.png',
+  },
+  {
+    id: 2,
+    title: 'We deliver your order wherever you are in Šibenk',
+    image: '/slide2.png',
+  },
+  {
+    id: 3,
+    title: 'The best pizza to share with your family',
+    image: '/slide3.jpg',
+  },
+];
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  /*   useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1));
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+ */
+  return (
+    <div className='flex flex-col  md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
+      {/* Text container */}
+      <div className='flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold '>
+        <h1 className='text-5xl text-center uppercase md:p-10 p-4 md:text-6xl xl:text-7xl'>
+          {data[currentSlide].title}
+        </h1>
+        <button className='bg-red-500 text-white py-4 px-8'>Order Now</button>
+      </div>
+
+      {/* Image container */}
+      <div className='w-full flex-1 relative'>
+        <Image
+          src={data[currentSlide].image}
+          alt=''
+          fill
+          className='object-cover'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
